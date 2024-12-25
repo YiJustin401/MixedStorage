29874ad9a77c9db688a50e04c174295722deed33
Shanfeng Pang <shanfengp@myscale.com>
2024-12-25 11:50:33 +0800

    Initialize btreeplus

--- src/storage/test/main.cpp
+++ src/storage/test/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 
 #include <storage/BTreePlus/BTreePlusStorage.h>
+#include <gtest/gtest.h>
 
-int main()
-{
+TEST(BTreePlusStorage, set) {
     MixS::BTreePlusStorage storage;
     storage.set("hello", "world");
     storage.set("hello1", "world1");
     storage.set("hello2", "world2");
     storage.set("hello3", "world3");
     storage.set("hello4", "world4");
-    std::cout << storage.get("hello1") << std::endl;
-    return 0;
+    EXPECT_EQ(storage.get("hello1"), "world1");
+    EXPECT_EQ(storage.get("hello2"), "world2");
+    EXPECT_EQ(storage.get("hello3"), "world3");
+    EXPECT_EQ(storage.get("hello5"), "null");
 }
